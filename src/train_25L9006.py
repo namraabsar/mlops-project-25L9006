9afd9e2358bf5affdd71e26a9dc1486e54275a0c
namraabsar <namra.absar@lhr.nu.edu.pk>
2026-09-05 17:43:17 +0500

    Added min-max scalling - 25L9006

--- src/train_25L9006.py
+++ src/train_25L9006.py
@@ -22,6 +22,8 @@ y = df.iloc[:, -1]
 # 2. Train a machine learning model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+X_train = (X_train - X_train.min()) / (X_train.max() - X_train.min())  # min-max scaling
+
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
